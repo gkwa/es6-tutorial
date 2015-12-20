b922484302bca66353f6fa153f3bd2d07ade8f69
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-12-19 16:22:52 -0800

    whitespace

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var calculateMonthlyPayment = function (principal, years, rate) {
-    let monthlyRate=0;
+    let monthlyRate = 0;
     if (rate) {
-        let monthlyRate = rate / 100 / 12;
+        monthlyRate = rate / 100 / 12;
     }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    let monthlyPayment = principal * monthlyRate /
+        (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
     return monthlyPayment;
 };
 
